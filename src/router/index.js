7a197938648e0ac88f1ca43aae0e392ff36c3895
janelisillaots <janelisillaots@gmail.com>
2025-11-17 18:29:45 +0200

    main page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import MainView from "../views/MainView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/main",
+    name: "main",
+    component: MainView,
   },
   {
     path: "/about",
---
+++ src/views/MainView.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>this page</h1>
+</template>
+
+<script>
+export default { name: "MainView" };
+</script>
